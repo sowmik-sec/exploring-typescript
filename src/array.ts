b3a62ea06201f2e09e457d70b956bd0a725d98e6
sowmik-sec <sowmik.sec@gmail.com>
2024-10-07 20:40:20 +0600

    array in ts

---
+++ src/array.ts
@@ -0,0 +1,8 @@
+const names: string[] = ["abul", "kabul", "babul"];
+const rollNumbers: number[] = [1, 2, 3, 4, 5, 6, 7];
+
+const arr = ["abul", 77, true];
+
+const user: [number, string] = [412, "kabul"];
+
+const partner: [string, string] = ["Evanka", "Netaneahu"];
