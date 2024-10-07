a293b9818f801bbbb18e3f254ce944577baaf15b
sowmik-sec <sowmik.sec@gmail.com>
2024-10-07 21:17:56 +0600

    Spread, Rest, Default Parameters and Destructuring in ts

--- src/function.ts
+++ src/function.ts
@@ -1,11 +1,17 @@
 //normal function
 
-function add(num1: number, num2: number): number {
+// default parameter
+function add(num1: number, num2: number = 35): number {
   return num1 + num2;
 }
 
-add(2, 3);
+add(3);
 
+// spread operator
+const myFriends = ["Danny", "Arya", "Khalisi"];
+const newFriends = ["John", "Robert", "Ramsay"];
+myFriends.push(...newFriends);
+// console.log(myFriends);
 const addArrow = (num1: number, num2: number): number => num1 + num2;
 
 const arrNum: number[] = [1, 3, 6];
@@ -23,3 +29,25 @@ const person: {
     console.log(`My new Balance is ${this.balance + money}`);
   },
 };
+
+// rest parameter
+
+const greetFriends = (...friends: string[]): void =>
+  friends.forEach((friend) => console.log(`Hi ${friend}`));
+
+greetFriends("Mormont", "Lyana", "Sansa");
+
+const [bestFriend] = myFriends;
+
+console.log(`My best friend is ${bestFriend}`);
+
+const myBestFriend: {
+  name: string;
+  age: number;
+} = {
+  name: "Theon Greyjoy",
+  age: 34,
+};
+
+const { name: bFriendName } = myBestFriend;
+console.log(bFriendName);
