c8ab413565b2affaf517b560dc1487b9743da982
sowmik-sec <sowmik.sec@gmail.com>
2024-10-07 20:35:45 +0600

    primitive types in ts

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
 const course: string = "Next level web development";
+const regNumber: number = 123;
+
 console.log(course);
+
+// Primitive types
+// string
+// number
+// boolean
+// null
+// undefined
