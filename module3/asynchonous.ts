056ddafddf8561e0383b5d0dff611e00488d600c
sowmik-sec <sowmik.sec@gmail.com>
2024-10-08 14:47:41 +0600

    Conditional Types in ts

--- module3/asynchonous.ts
+++ module3/asynchonous.ts
@@ -1,70 +1,70 @@
-// mocking
-const makePromise = (): Promise<string> => {
-  return new Promise<string>((resolve, reject) => {
-    const data = "Data is fetched";
-    if (data) {
-      resolve(data);
-    } else {
-      reject("Failed to fetch data!");
-    }
-  });
-};
+// // mocking
+// const makePromise = (): Promise<string> => {
+//   return new Promise<string>((resolve, reject) => {
+//     const data = "Data is fetched";
+//     if (data) {
+//       resolve(data);
+//     } else {
+//       reject("Failed to fetch data!");
+//     }
+//   });
+// };
 
-const makePromiseBoolean = (): Promise<boolean> => {
-  return new Promise<boolean>((resolve, reject) => {
-    const data: boolean = true;
-    if (data) {
-      resolve(data);
-    } else {
-      reject("Failed to fetch data!");
-    }
-  });
-};
+// const makePromiseBoolean = (): Promise<boolean> => {
+//   return new Promise<boolean>((resolve, reject) => {
+//     const data: boolean = true;
+//     if (data) {
+//       resolve(data);
+//     } else {
+//       reject("Failed to fetch data!");
+//     }
+//   });
+// };
 
-type DataType = {
-  data: string;
-};
+// type DataType = {
+//   data: string;
+// };
 
-const makePromiseObject = (): Promise<DataType> => {
-  return new Promise<DataType>((resolve, reject) => {
-    const data: DataType = { data: "world" };
-    if (data) {
-      resolve(data);
-    } else {
-      reject("Failed to fetch data!");
-    }
-  });
-};
+// const makePromiseObject = (): Promise<DataType> => {
+//   return new Promise<DataType>((resolve, reject) => {
+//     const data: DataType = { data: "world" };
+//     if (data) {
+//       resolve(data);
+//     } else {
+//       reject("Failed to fetch data!");
+//     }
+//   });
+// };
 
-interface ITodo {
-  userId: number;
-  id: number;
-  title: string;
-  completed: boolean;
-}
+// interface ITodo {
+//   userId: number;
+//   id: number;
+//   title: string;
+//   completed: boolean;
+// }
 
-const getTodo = async (): Promise<ITodo> => {
-  const response = await fetch(`https://jsonplaceholder.typicode.com/todos/1`);
-  const todo = (await response.json()) as ITodo;
-  return todo;
-};
+// const getTodo = async (): Promise<ITodo> => {
+//   const response = await fetch(`https://jsonplaceholder.typicode.com/todos/1`);
+//   const todo = (await response.json()) as ITodo;
+//   return todo;
+// };
 
-const getTodoData = async (): Promise<void> => {
-  const result = await getTodo();
-  console.log(result);
-};
-getTodoData();
+// const getTodoData = async (): Promise<void> => {
+//   const result = await getTodo();
+//   console.log(result);
+// };
+// getTodoData();
 
-const getPromiseData = async (): Promise<string> => {
-  const data = await makePromise();
-  return data;
-};
+// const getPromiseData = async (): Promise<string> => {
+//   const data = await makePromise();
+//   return data;
+// };
 
-const getPromiseDataBoolean = async (): Promise<boolean> => {
-  const data = await makePromiseBoolean();
-  return data;
-};
-const getPromiseDataObject = async (): Promise<DataType> => {
-  const data = await makePromiseObject();
-  return data;
-};
+// const getPromiseDataBoolean = async (): Promise<boolean> => {
+//   const data = await makePromiseBoolean();
+//   return data;
+// };
+// const getPromiseDataObject = async (): Promise<DataType> => {
+//   const data = await makePromiseObject();
+//   return data;
+// };
