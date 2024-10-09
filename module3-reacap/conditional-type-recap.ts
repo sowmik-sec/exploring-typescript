2d69fd8c138921ce70d056073205dad33452cd5e
sowmik-sec <sowmik.sec@gmail.com>
2024-10-09 12:01:48 +0600

    conditional type recap

---
+++ module3-reacap/conditional-type-recap.ts
@@ -0,0 +1,28 @@
+type a1 = string;
+type a3 = undefined;
+type a4 = number;
+type a2 = a1 extends string ? string : null;
+
+//nested conditional type
+type d = a1 extends null
+  ? null
+  : a3 extends number
+  ? number
+  : a4 extends null
+  ? null
+  : never;
+
+type Sheikh = {
+  wife1: string;
+  wife2: string;
+};
+
+type CheckProperty<T, K> = K extends keyof T ? true : false;
+
+type CheckWife1 = CheckProperty<Sheikh, "wife1">;
+
+type Bandhobi = "Alexa" | "Lexi" | "Lena";
+
+type RemoveBandhobi<T, K> = T extends K ? never : T;
+
+type CurrentBandhobi = RemoveBandhobi<Bandhobi, "Lexi">;
