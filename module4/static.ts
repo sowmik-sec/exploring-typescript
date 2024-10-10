3552cf0413f964e53eaab339c8fe6d669ac78849
sowmik-sec <sowmik.sec@gmail.com>
2024-10-10 11:51:22 +0600

    Static in Class

---
+++ module4/static.ts
@@ -0,0 +1,20 @@
+class Counter {
+  static counter: number = 0;
+
+  static increment(): number {
+    return ++Counter.counter;
+  }
+  static decrement(): number {
+    return ++Counter.counter;
+  }
+}
+
+const instance1 = new Counter();
+const instance2 = new Counter();
+
+Counter.increment();
+Counter.increment();
+Counter.increment();
+Counter.increment();
+
+console.log(Counter.counter);
