af92452b87c905da07d933fae1015d8522a336c3
sowmik-sec <sowmik.sec@gmail.com>
2024-10-13 11:36:40 +0600

    exercise 5 solution

---
+++ practice/exercise5.ts
@@ -0,0 +1,11 @@
+function addEvenNumber<T extends number>(arr: T[]): number {
+  let ans = 0;
+  for (const num of arr) {
+    if (num % 2 === 0) {
+      ans += num;
+    }
+  }
+  return ans;
+}
+
+console.log(addEvenNumber([1, 2, 3, 4, 5, 6]));
