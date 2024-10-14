a24b786c0d81ee33ba0f490d866cc9c7d6866396
sowmik-sec <sowmik.sec@gmail.com>
2024-10-14 12:07:24 +0600

    solution 1

---
+++ assignment/solution1.ts
@@ -0,0 +1,7 @@
+// const printMultipleTimes = (str: string, n: number = 3): void => {
+//   for (let i = 0; i < n; i++) {
+//     console.log(str);
+//   }
+// };
+
+// printMultipleTimes("fuck", 6);
