42c5d3863b16a9b21677309bd14e15b71243a498
sowmik-sec <sowmik.sec@gmail.com>
2024-10-15 11:21:51 +0600

    optional solution 1

---
+++ assignment/optional-solution1.ts
@@ -0,0 +1,13 @@
+// let str: string[] = [];
+// let tuple: [number, string];
+
+// str.push("2wfasd");
+// str.push("hfds");
+// str.push("5fs");
+// str.push("jf2");
+// str.push("jhf2");
+
+// tuple = [324, "fasdf"];
+
+// console.log(str);
+// console.log(tuple);
--- assignment/solution5.ts
+++ assignment/solution5.ts
@@ -1,9 +1,9 @@
-const printString = (s: unknown): void => {
-  if (typeof s === "string") {
-    console.log(s);
-  } else {
-    console.log("You must provide string as input");
-  }
-};
+// const printString = (s: unknown): void => {
+//   if (typeof s === "string") {
+//     console.log(s);
+//   } else {
+//     console.log("You must provide string as input");
+//   }
+// };
 
-printString("false");
+// printString("false");
